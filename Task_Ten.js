fcd830394cb0457386ce4285ef11e6e807ff2ead
Re4derTheGlorious <karol.re4der@gmail.com>
2023-02-02 16:19:22 +0100

    Task_Ten solved

---
+++ Task_Ten.js
@@ -0,0 +1,27 @@
+﻿function fixage(arr) {
+    var result = ''
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] >= 18 && arr[i] <= 60) {
+            if (result !== '') {
+                result += ','
+            }
+            result += arr[i]
+        }
+    }
+
+    if (result === '') {
+        return 'NA'
+    }
+
+    return result
+}
+
+//For testing
+//var result = fixage([5, 15, 25, 78, 59, 45])
+//console.log(result)
+//result = fixage([18, 3, 30, 22, 11, 60])
+//console.log(result)
+//result = fixage([1, 3, 3, 2, 11, 6])
+//console.log(result)
+//result = fixage([])
+//console.log(result)
